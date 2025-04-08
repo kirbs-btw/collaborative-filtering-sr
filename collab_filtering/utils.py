b57c74fef98c1da29de8f41bdb39507ff8656d79
Bastian Lipka <lipka.bastian04@gmail.com>
2025-04-09 00:13:09 +0200

    implemented basic training and the class for cf

--- collab_filtering/utils.py
+++ collab_filtering/utils.py
@@ -3,27 +3,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MultiLabelBinarizer
 
 class placeholder: 
-    def __init__(self):
-        ...
+    def __init__(self, data: list[list[str]]):
+        self.__mlb = MultiLabelBinarizer()
+        self.item_similarity = self.train(data)
 
 
     def train(self, data: list[list[str]]):
-        mlb = MultiLabelBinarizer()
-        user_item_matrix = pd.DataFrame(mlb.fit_transform(data), columns=mlb.classes_)
+        user_item_matrix = pd.DataFrame(self.__mlb.fit_transform(data), columns=self.__mlb.classes_)
 
-        item_similarity = pd.DataFrame(
+        return pd.DataFrame(
             cosine_similarity(user_item_matrix.T),
             index=user_item_matrix.columns,
             columns=user_item_matrix.columns
         )
 
+    def recommend_items(self, target_data: list[str]):
+        new_vector = pd.Series(self.__mlb.transform([target_data])[0], index=self.__mlb.classes_)
+        scores = self.item_similarity.dot(new_vector)
+        scores = scores[~new_vector.astype(bool)]  # Remove already seen items
+        recommended_items = scores.sort_values(ascending=False)
+        return recommended_items
 
-    def recommend(self, target_data):
-
-        # need a way to get the vector of the data in the cf topic 
-        # user_vector = user_item_matrix.iloc[user_id]
-        # scores = item_similarity.dot(user_vector)
-        # scores = scores[~user_vector.astype(bool)]  # Remove already seen items
-
-        # recommended_items = scores.sort_values(ascending=False)
-        pass
+    def recommend(self, target_data: list[str]):
+        return self.recommend_items[0]
